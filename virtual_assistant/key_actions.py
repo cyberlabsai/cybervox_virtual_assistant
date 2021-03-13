25c0ce1e302f323f881292085a6215a70df1fd48
atilarangel <atila.rangel@cyberlabs.com.br>
2021-03-13 17:30:19 -0300

    Add TTS response

--- virtual_assistant/key_actions.py
+++ virtual_assistant/key_actions.py
@@ -1,21 +1,30 @@
 import requests
 from jinja2 import Template
 
+from virtual_assistant.key_local_actions import local_actions
 from virtual_assistant.utils import log
 from virtual_assistant.utils import config
 
 logger = log.logger
+
 def send(action):
     try:
         logger.info('Send action')
         headers = {'content-type': 'application/json'}
-        response = requests.request(action['method'], action['url'], headers=headers)
+        body = {}
+
+        if action['staticPayload']  != None:
+            body = action['staticPayload']
+        response = requests.request(action['method'], action['url'], headers=headers, json=body)
         if response.status_code != 200:
             logger.warning("Status code %i" % response.status_code)
-    except:
-        logger.error("Ops! Error :/")
+    except Exception as err:
+        logger.error("Ops! Error :/ %s" % str(err))
+
 def get():
     logger.info('Requesting portals actions')
+    if config.key_token == "":
+        return local_actions
     with open('virtual_assistant/templates/portal.json.jinja') as f:
         payload = Template(f.read())
     body = payload.render(key_token=config.key_token, key_portal=config.key_portal)
