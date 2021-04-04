bdebb2dba193b1e0369a339d939933548ce870fb
atilarangel <atila.rangel@cyberlabs.com.br>
2021-04-03 21:10:10 -0300

    Better local examples

--- bin/simple_server_test.py
+++ bin/simple_server_test.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/<data>', methods=['GET'])
 def server(data):
-    print('From Virtual Assistant > ', data)
-    return { "action": data, "status": True, "text": "Pou pou!" }
+    print('From virtual assistant', data)
+    return {}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='8080')
